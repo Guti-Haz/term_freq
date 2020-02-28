86988818fd4e51122feaea3e35bc93462536c965
Guti-Haz <30297859+Guti-Haz@users.noreply.github.com>
2020-02-28 09:54:22 +0800

    Update script.R

--- script.R
+++ script.R
@@ -1,19 +1,18 @@
-pacman::p_load(randomNames,tm,stringr,parallel,tictoc,pbapply,data.table,magrittr)
-names=randomNames(400000)%>%
-  str_replace_all("[:punct:]","")%>%
-  str_to_lower
-res=termFreq(names)
-res.dt=data.table(terms=names(res),res=res)
-# vector subsetting - base + linux
-tic()
-score=str_split(names[1:4000]," ")%>%
-  pblapply(function(x){sum(res[x])})
-toc()
-# data.table filter - base + linux
-tic()
-score=str_split(names," ")%>%
-  lapply(function(x){
-    res.dt[terms%chin%x,sum(res)]
-  })%>%
-  lapply(sum)
-toc()
+pacman::p_load(data.table,magrittr,stringr,textclean,tibble,tm,tictoc,pbapply)
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+tf=function(dt){
+  res=termFreq(dt[,unique(names)])
+  temp=data.table(terms=str_to_upper(names(res)),
+                  termFreq=res)%>%
+    .[,prob:=termFreq/.[,sum(termFreq)]]%>%
+    .[,termFreq:=NULL]
+  return(temp)
+}
+dt=readRDS("Names.rds")%>%
+  str_split(" ")%>%
+  lapply(1:length(.),function(i,l){data.table(terms=l[[i]],names=Names[i])},l=.)%>%
+  rbindlist%>%
+  .[terms!=""]%>%
+  merge(tf(.),by="terms",all.x=T)%>%
+  .[!is.na(prob)]%>%
+  .[,.(score=prod(prob)),names]
